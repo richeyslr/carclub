8f63914ffb3df664b107ba61b541cdf743d6598b
Spencer Richey <richeyslr@gmail.com>
2021-06-30 15:06:19 -0700

    made js file to try scripts

---
+++ script.js
@@ -0,0 +1,5 @@
+alert("You can now change the background color to gold");
+var colorChange = prompt("What color do you want your background?");
+if (colorChange == gold){
+    homepage.body.style.backgroundColor == "purple"
+};
