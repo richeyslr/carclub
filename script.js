d994b27ce99616aa0e0296defab1996f318d26a2
Spencer Richey <richeyslr@gmail.com>
2021-07-01 12:31:37 -0700

    made background changer a function

--- script.js
+++ script.js
@@ -1,6 +1,18 @@
-alert("You can now change the background color to gold");
-var colorChange = prompt("What color do you want your background?");
-if (colorChange == 'gold'){
-    console.log('making sure its gold');
-    document.body.style.backgroundColor = 'gold';
+alert("You can now change the background color to gold, purple, red, or blue");
+var backgroundChoice = prompt("What color do you want your background?");
+
+function backgroundChanger(){
+    if (backgroundChoice == 'gold'){
+        document.body.style.backgroundColor = 'gold';
+    }
+    else if (backgroundChoice == 'purple'){
+        document.body.style.backgroundColor = 'purple';
+    }
+    else if (backgroundChoice == 'red'){
+        document.body.style.backgroundColor = 'red';
+    }
+    else if (backgroundChoice == 'blue'){
+        document.body.style.backgroundColor = 'blue';
+    }
 }
+backgroundChanger();
