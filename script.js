e25dc4ba50b6182bf572c492df493b5ab36dc25e
Spencer Richey <richeyslr@gmail.com>
2021-07-01 11:16:18 -0700

    fixed background color issue

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 alert("You can now change the background color to gold");
 var colorChange = prompt("What color do you want your background?");
-if (colorChange == gold){
-    homepage.body.style.backgroundColor == "purple"
-};
+if (colorChange == 'gold'){
+    console.log('making sure its gold');
+    document.body.style.backgroundColor = 'gold';
+}
