008801fdaf133ca2c7e2e37ff25343d91f95b6ba
Spencer Richey <richeyslr@gmail.com>
2021-07-01 18:17:59 -0700

    added loop to background changer if unknown color is selected

--- script.js
+++ script.js
@@ -1,18 +1,29 @@
-alert("You can now change the background color to gold, purple, red, or blue");
-var backgroundChoice = prompt("What color do you want your background?");
-
 function backgroundChanger(){
+    alert("You can now change the background color to gold, purple, red, or blue.");
+    var backgroundChoice = prompt("What color do you want your background?" );
     if (backgroundChoice == 'gold'){
         document.body.style.backgroundColor = 'gold';
+        return true;
     }
     else if (backgroundChoice == 'purple'){
         document.body.style.backgroundColor = 'purple';
+        return true;
     }
     else if (backgroundChoice == 'red'){
         document.body.style.backgroundColor = 'red';
+        return true;
     }
     else if (backgroundChoice == 'blue'){
         document.body.style.backgroundColor = 'blue';
+        return true;
+    }
+    else {
+        alert ("I didn't have time for that color. Please choose a listed color.");
+        return false;
     }
 }
+
 backgroundChanger();
+while(backgroundChanger() == false){
+backgroundChanger();
+}
